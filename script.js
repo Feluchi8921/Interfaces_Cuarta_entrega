fa07901a5fba4238c3977920b60b3f4a7d8d3ec9
Felicitas <felicitas.aguerralde@gmail.com>
2023-11-17 17:48:01 -0300

    Agrego loader

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simular una carga de 5 segundos
+    setTimeout(function () {
+        // Ocultar el loader
+        document.getElementById("loaderContainer").style.display = "none";
+        // Mostrar el contenido principal
+        document.getElementById("content").style.display = "block";
+    }, 5000);
+
+    // Actualizar el porcentaje de carga cada segundo
+    let progress = 0;
+    const interval = setInterval(function () {
+        progress += 20;
+        document.getElementById("loader").style.borderBottomColor = "#3498db";
+        document.getElementById("loaderText").innerText = `Cargando... ${progress}%`;
+
+        if (progress >= 100) {
+            clearInterval(interval);
+        }
+    }, 1000);
+});
