a30241b5d9a29cf93da7981b554b967af8c3d78b
Felicitas <felicitas.aguerralde@gmail.com>
2023-11-23 11:14:07 -0300

    Agrego animaciones a la portada

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const menuAchicar = document.querySelectorAll(".menuAchicar");
 const achicarMenuHamburguesa = document.querySelectorAll(".achicarMenuHamburguesa");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY >= 600) {
+  if (window.scrollY >= 200) {
     menuAchicar.forEach(element => {
       element.classList.add("achicar");
     });
@@ -57,3 +57,51 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
+/*-------------------Parallax-------------*/
+
+const spidey = document.querySelector("#spidey");
+const edificioIzq = document.querySelector("#edificioIzq");
+const edificio = document.querySelector("#edificio");
+const edificioDer = document.querySelector("#edificioDer");
+const chica = document.querySelector("#chica");
+const spideyNegro = document.querySelector("#spideyNegro");
+const spideyCentral = document.querySelector("#spideyCentral");
+const cuerdaDerecha = document.querySelector("#cuerdaDerecha");
+const cuerdaIzquierda = document.querySelector("#cuerdaIzquierda");
+
+chica.style.opacity=0;
+spideyCentral.style.opacity=0;
+spideyNegro.style.opacity=0;
+cuerdaDerecha.style.opacity=0;
+cuerdaIzquierda.style.opacity=0;
+
+cuerdaIzquierda.style.opacity=0;
+cuerdaDerecha.style.opacity=0;
+
+window.addEventListener("scroll", ()=>{
+    let scroll = window.scrollY;
+    if(scroll>=0 && scroll<200){
+        chica.style.opacity = scroll * 0.5;
+        spideyCentral.style.opacity = scroll * 0.5;
+        spideyNegro.style.opacity = scroll * 0.5;
+        cuerdaDerecha.style.opacity = scroll * 0.8;
+        cuerdaIzquierda.style.opacity = scroll * 0.8;
+    }
+    if(scroll>=200 && scroll<=350){
+        cuerdaIzquierda.style.opacity=0;
+        cuerdaDerecha.style.opacity=0;
+
+        chica.style.left = scroll * 3 + "px";
+        chica.style.top = scroll * 3 + "px";
+
+        
+        spideyCentral.style.left = scroll * 2 + "px";
+
+        
+        spideyNegro.style.left = scroll * 3 + "px";
+        spideyNegro.style.skew = scroll*0.1 +"deg";
+
+    }
+    
+})
